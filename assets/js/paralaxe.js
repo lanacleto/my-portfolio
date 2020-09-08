ed4171a74fbaaa89faff079a2dea0cf8d12720a8
Luiz Fernando Anacl <luizfernandoanacleto68@gmail.com>
2020-09-08 07:47:50 -0300

    add paralaxe and about files

--- assets/js/paralaxe.js
+++ assets/js/paralaxe.js
@@ -1,8 +1,12 @@
 const img = document.querySelector('.img-about')
+const aboutMe = document.querySelector ('#about-me')
 
 window.addEventListener('scroll', function(e){
     var value = window.scrollY
 
     img.style.top = value * 0.5 + 'px'
 
-})
+    }
+)
+
+
